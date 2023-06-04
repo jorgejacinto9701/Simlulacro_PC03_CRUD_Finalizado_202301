c5a4ec4b62d59cf65fa8e6660532dc8025d301a1
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-06-03 21:29:38 -0500

    segundo commit

--- app/src/main/java/com/example/semana10_simulacro_pc03/ClienteCrudListaActivity.java
+++ app/src/main/java/com/example/semana10_simulacro_pc03/ClienteCrudListaActivity.java
@@ -1,14 +1,31 @@
 package com.example.semana10_simulacro_pc03;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.semana10_simulacro_pc03.util.NewAppCompatActivity;
 
 public class ClienteCrudListaActivity extends NewAppCompatActivity {
 
+    Button  btnCrudListar, btnCrudRegistra;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cliente_crud_lista);
+
+        btnCrudListar = findViewById(R.id.btnCrudListar);
+        btnCrudRegistra = findViewById(R.id.btnCrudRegistra);
+
+        btnCrudRegistra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ClienteCrudListaActivity.this, ClienteCrudFormularioActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
