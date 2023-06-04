b29fb33a9d056a69b72fa342f2b3ab8e217db2b7
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-06-03 22:31:35 -0500

    cuarto commit

--- app/src/main/java/com/example/semana10_simulacro_pc03/ClienteCrudFormularioActivity.java
+++ app/src/main/java/com/example/semana10_simulacro_pc03/ClienteCrudFormularioActivity.java
@@ -6,16 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.semana10_simulacro_pc03.util.NewAppCompatActivity;
 
 public class ClienteCrudFormularioActivity extends NewAppCompatActivity {
 
-    Button btnCrudRegresar;
+    Button btnCrudRegresar , btnCrudRegistra;
+    TextView txtTitulo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cliente_crud_formulario);
+
+        btnCrudRegistra = findViewById(R.id.btnCrudRegistrar);
+        txtTitulo = findViewById(R.id.idCrudTituloCliente);
+
+        Bundle extras = getIntent().getExtras();
+        String tipo = (String) extras.get("var_tipo");
+        String titulo = (String) extras.get("var_titulo");
+
+        btnCrudRegistra.setText(tipo);
+        txtTitulo.setText(titulo);
 
         btnCrudRegresar = findViewById(R.id.btnCrudRegresar);
         btnCrudRegresar.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/semana10_simulacro_pc03/ClienteCrudListaActivity.java
+++ app/src/main/java/com/example/semana10_simulacro_pc03/ClienteCrudListaActivity.java
@@ -3,13 +3,35 @@ package com.example.semana10_simulacro_pc03;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridView;
 
+import com.example.semana10_simulacro_pc03.adapter.ClienteCrudAdapter;
+import com.example.semana10_simulacro_pc03.entity.Cliente;
+import com.example.semana10_simulacro_pc03.service.ServiceCliente;
+import com.example.semana10_simulacro_pc03.util.ConnectionRest;
 import com.example.semana10_simulacro_pc03.util.NewAppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ClienteCrudListaActivity extends NewAppCompatActivity {
 
     Button  btnCrudListar, btnCrudRegistra;
+
+    //GridView
+    GridView gridCrudClientes;
+    ArrayList<Cliente> data = new ArrayList<Cliente>();
+    ClienteCrudAdapter adaptador;
+
+    //service
+    ServiceCliente serviceCliente;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +39,11 @@ public class ClienteCrudListaActivity extends NewAppCompatActivity {
 
         btnCrudListar = findViewById(R.id.btnCrudListar);
         btnCrudRegistra = findViewById(R.id.btnCrudRegistra);
+        gridCrudClientes = findViewById(R.id.gridCrudClientes);
+        adaptador = new ClienteCrudAdapter(this,R.layout.activity_cliente_crud_item,data);
+        gridCrudClientes.setAdapter(adaptador);
+
+        serviceCliente =  ConnectionRest.getConnection().create(ServiceCliente.class);
 
         btnCrudRegistra.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,10 +51,50 @@ public class ClienteCrudListaActivity extends NewAppCompatActivity {
                 Intent intent = new Intent(
                         ClienteCrudListaActivity.this,
                         ClienteCrudFormularioActivity.class);
+                intent.putExtra("var_titulo", "REGISTRA CLIENTE");
+                intent.putExtra("var_tipo", "REGISTRA");
                 startActivity(intent);
             }
         });
 
+        btnCrudListar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    lista();
+            }
+        });
 
+        gridCrudClientes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent = new Intent(
+                        ClienteCrudListaActivity.this,
+                        ClienteCrudFormularioActivity.class);
+                intent.putExtra("var_titulo", "ACTUALIZA CLIENTE");
+                intent.putExtra("var_tipo", "ACTUALIZA");
+                startActivity(intent);
+
+            }
+        });
     }
+
+    public void lista(){
+        Call<List<Cliente>> call = serviceCliente.listaCliente();
+        call.enqueue(new Callback<List<Cliente>>() {
+            @Override
+            public void onResponse(Call<List<Cliente>> call, Response<List<Cliente>> response) {
+                List<Cliente> lista =response.body();
+                data.clear();
+                data.addAll(lista);
+                adaptador.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<List<Cliente>> call, Throwable t) {
+
+            }
+        });
+    }
+
 }
